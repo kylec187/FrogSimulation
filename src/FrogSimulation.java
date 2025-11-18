f0d47dbec62db1f7671914d3902e58f250d5d249
KyleChan <kylec187@nycstudents.net>
2025-11-18 13:16:14 -0500

    Worked more on the methods for FrogSimulation

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -6,10 +6,15 @@ public class FrogSimulation {
         maxHops = numHops;
     }
     private int hopDistance() {
-
+        if (Math.random() < 0.5) {
+            return (int)(Math.random()*14+1);
+        }
+        else return (int)-(Math.random()*14+1);
     }
     public boolean simulate() {
-        return hopDistance() >= goalDistance;
+        int distance = 0;
+        for(int i = 0; i < maxHops; i++) distance += hopDistance();
+        return distance >= goalDistance;
     }
     public double runSimulations(int num) {
 
